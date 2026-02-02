80b57fd8fb65e25fbbc4d117ec95a97578bad5f2
jmpizza <jespitiap@unal.edu.co>
2026-02-01 22:39:50 -0500

    Commenting the testing code of app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,17 +1,19 @@
 package app
 
-import (
-	"fmt"
-
-	"github.com/jmpizza/Flower-Trick/internal/handlers"
-)
+// import (
+// 	//"github.com/jmpizza/Flower-Trick/internal/handlers"
+// )
 
 func Run() {
 	// Routing
 	r := NewRouter()
 
-	fmt.Println(handlers.Ability("-1"))
-
+	//fmt.Println(handlers.Ability("1"))
+	// //data, err := handlers.Pokemon("meowscarada")
+	//data, err := handlers.Characteristic()
+	// fmt.Printf("%#v\n\n\n\n", data)
+	// fmt.Println(data)
+	// fmt.Println(err)
 	// Server start
 	Start(r)
 
